160d81606bad7485e5c1bc0e994997450b480658
Vadim <support@zavodil.ru>
2026-08-01 16:01:35 +0300

    repo renamed

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@ set -e
 
 cd "$(dirname "$0")"
 
-echo "Building env-test-ark for WASI P1..."
+echo "Building env-test-example for WASI P1..."
 cargo build --target wasm32-wasip1 --release
 
 # Copy to current dir for convenience
-cp ./target/wasm32-wasip1/release/env-test-ark.wasm ./env-test-ark.wasm
+cp ./target/wasm32-wasip1/release/env-test-example.wasm ./env-test-example.wasm
 
-echo "Done! Output: env-test-ark.wasm"
-ls -lh env-test-ark.wasm
+echo "Done! Output: env-test-example.wasm"
+ls -lh env-test-example.wasm
